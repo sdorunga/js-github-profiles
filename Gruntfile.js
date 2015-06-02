3aa946851fb24f81e774fd96befb23ce1c20d99a
Stefan Dorunga <sdorunga@housetrip.com>
2015-06-02 15:39:01 +0100

    Add angular

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    jshint: {
+      all: ["Gruntfile.js", "src/**/*.js", "spec/**/*.js"] 
+    },
+    watch: {
+      scripts: {
+        files: ["Gruntfile.js", "src/**/*.js", "spec/**/*.js"],
+        tasks: ["jshint", "jasmine"]
+      }
+    }
+  });  
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-jasmine");
+  grunt.registerTask("default", ["jshint"]);
+};
